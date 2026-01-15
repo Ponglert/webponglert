bf59242be0ff887de2de29d8962aa5cb0f788718
Ponglert <asakuratran@gmail.com>
2026-01-15 15:43:32 +0700

    upv2

--- from1.php
+++ from1.php
@@ -7,11 +7,11 @@
             <input type="text" name="user" size="25" maxlength="25"> <br>
             <input type="password" name="Pwd" size="8" maxlength="8"> <br>
             <textarea type="text" name="address" cols="60" rows="5"></textarea> <br>
-            <input type="radio" name="gender" value="meal"/>ชาย
-            <input type="radio" name="gender" value="female"/>หญิง
+            <input type="radio" name="gender" value="Male"/>ชาย
+            <input type="radio" name="gender" value="Female"/>หญิง
             <br>
             <input type="checkbox" name="Hobby" value="อ่านหนังสือ"> อ่านหนังสือ
-            <input type="checkbox" name=" Hobby " value="ดูทีวี"> ดูทีวี
+            <input type="checkbox" name="Hobby" value="ดูทีวี"> ดูทีวี
             <select name="beverage">
             <option value="IcedTea" >ICED TEA </option>
             <option value="LemonTea"> LEMON TEA </option>
--- from2.php
+++ from2.php
@@ -3,16 +3,26 @@
         <title>from2</title>
     </head>
     <body>
+        
         <?php
             if(isset($_POST['save'])){
                 $user = $_POST['user'];
                 $pwd = $_POST['Pwd'];
+                $address = $_POST['address'];
+                $gender = $_POST['gender'];
+                $beverage = $_POST['beverage'];
                 echo "$user <br>";
                 echo "$pwd <br>";
+                echo "$address <br>";
+                echo "$gender <br>";
+                echo "$beverage <br>";
             }
             else{
                 echo "You didn't press save.";
             }
         ?>
+        <form name= "frmRegis" method= "post" action= "from1.php">
+            <input type= "submit" value= "Back">
+        </form>
     </body>
 </html>
